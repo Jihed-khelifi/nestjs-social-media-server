c5408f3599af6061c6a7f58fd729e661903f8e57
Mayank <mayankgirdhar0001@gmail.com>
2022-08-08 19:29:30 +0530

    user not found exception

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
 import { jwtConstants } from './constants';
 import { UsersService } from '../users/users.service';
 
@@ -9,13 +9,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private userservice: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: jwtConstants.secret,
     });
   }
 
   async validate(payload: any) {
     const user = await this.userservice.findByEmail(payload.email);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
     const { password, ...result } = user;
     return result;
   }
